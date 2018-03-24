a2b8a7a9b546aa6bd6654cbcbb95da216e06cab2
Mike Matheny <mike.matheny.81@gmail.com>
2018-03-24 13:15:44 -0400

    working copy

--- assets/javascript/advertisement.js
+++ assets/javascript/advertisement.js
@@ -1,3 +1,8 @@
+//still need to create variable for youtube video id for use in APT
+//still need button to hide details div and show posts-table div
+
+
+
 var config = {
   apiKey: "AIzaSyC22pg8At8rnWwjY-8HhGMlL8RSqtIO-lw",
   authDomain: "mo-bands-mo-problems.firebaseapp.com",
@@ -10,13 +15,6 @@ firebase.initializeApp(config);
 
 var bandData = firebase.database();
 
-
-
-
-
-
-
-
 bandData.ref().on("child_added", function (childSnapshot, prevChildKey) {
 
   console.log(childSnapshot.val());
@@ -27,6 +25,7 @@ bandData.ref().on("child_added", function (childSnapshot, prevChildKey) {
   var bDate = childSnapshot.val().need_date;
   var bDes = childSnapshot.val().description;
   var bMedia = childSnapshot.val().media;
+  var bLoc = childSnapshot.val().location;
 
   console.log(bName);
   console.log(bNeed);
@@ -34,24 +33,31 @@ bandData.ref().on("child_added", function (childSnapshot, prevChildKey) {
   console.log(bDes);
   console.log(bMedia);
 
-  var heading = $("<h1>");
-  heading.text(bNeed);
-  $("#posts-table > tbody").append("<tr><td>" + bName + "</td><td>" + bNeed + "</td><td>" +
-    bDate + "</td><td>" + bDes + "</td><td>" + bMedia + "</td></tr>");
 
-  heading.on("click", function () {
-    // Load the details for this ad
-    $("#details-desc").text(bDes);
-    // Hide the main table
-    $("#wrapper").hide();
-    // Show the details page
-    $("#details").show();
-  })
-  //$("#wrapper").append(heading);
-  //$("#wrapper").append("<p>" + bName + "</p><<p>" + bDes + "</p><p>" + bDate + "</p>");
+  var tr = $("<tr>");
+  var headingTD = $("<td>");
+  headingTD.text(bNeed).attr("class", "heading").attr("data-bDes", bDes).attr("data-bName", bName).attr("data-bNeed", bNeed).attr("data-bDate", bDate).attr("data-bMedia", bMedia);
+
+
+
+  tr.append(headingTD)
+ $("#posts-table").append(tr)
+
 })
 
+$("body").on("click", ".heading", function () {
 
+  // Load the details for this ad
+  $("#name-div").text($(this).attr("data-bName"));
+  $("#need-div").text($(this).attr("data-bNeed"));
+  $("#date-div").text($(this).attr("data-bDate"));
+  $("#desc-div").text($(this).attr("data-bDes"));
+  //$("#loc-div").text($(this).attr("data-bLoc"));
+  // Hide the main table
+  $("#wrapper").hide();
+  // Show the details page
+  $("#details").show();
+});
 
 
   // 2. This code loads the IFrame Player API code asynchronously.
@@ -59,6 +65,7 @@ function displayVideo() {
   //var media = childSnapshot.val().media;
   //bandData?
 
+  var id = $(this).attr("data-bMedia");
 
   var tag = document.createElement('script');
 
@@ -73,7 +80,7 @@ function displayVideo() {
     player = new YT.Player('player', {
       height: '300',
       width: '300',
-      videoId: '7XwdMWXz7QA',
+      videoId: 'id',
       playerVars: {
 
       },
@@ -90,3 +97,5 @@ function displayVideo() {
 
   }
 }
+
+
